54b40a53e18c326e1d1632e06ecc089211b80d16
Ishan Pattnayak <ishanpattnayak@gmail.com>
2021-09-02 21:42:22 +0530

    Handling 404 error

--- app.js
+++ app.js
@@ -50,6 +50,11 @@ app.post('/contact', (req, res)=>{
     // res.status(200).render('contact.pug');
 })
 
+// Handling 404 error
+app.use((req, res, next) =>{
+    res.status(404).send("404 error ! Page not found")
+})
+
 // START THE SERVER
 app.listen(port, ()=>{
     console.log(`The application started successfully on port ${port}`);
